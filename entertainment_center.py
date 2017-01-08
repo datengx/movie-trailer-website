fbf00208307a7ea4a9c072fd4ccbe511e58af6a1
Da Teng <da_teng0702@hotmail.com>
2017-01-07 16:32:29 -0800

    Init commit

---
+++ entertainment_center.py
@@ -0,0 +1,59 @@
+import media
+import fresh_tomatoes
+
+# Create a list to store movie instances
+movie_list = [];
+
+# Create multiple movie instances
+toy_story = media.Movie(
+						"Toy Story",
+						81,
+						"A story of a boy and his toys that come to life",
+						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+						"https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie(
+					"Avatar",
+					162,
+					"A marine on an alien planet",
+					"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+					"http://www.youtube.com/watch?v=-9ceBgWV8io")
+
+starwar_2015 = media.Movie(
+							"Star War 2015",
+						  135,
+						  "Visionary director J.J. Abrams brings to life the motion picture event of a generation.",
+						  "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",
+						  "https://www.youtube.com/watch?v=-eYxeRJCYMY")
+
+school_of_rock = media.Movie(
+							"School of Rock",
+							109,
+							"Using rock music to learn",
+							"https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+							"https://www.youtube.com/watch?v=LqEszt5wG9I")
+
+ratatouille = media.Movie(
+						"Ratatouille",
+						115,
+						"A rat is a chef in Paris",
+						"https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+						"https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie(
+							"Midnight in Paris",
+							100,
+							"Going back in time to meet authors",
+							"https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+							"https://www.youtube.com/watch?v=BYRWfS2s2v4")
+# Put the created motive instances into the list
+movie_list.append(toy_story)
+movie_list.append(avatar)
+movie_list.append(starwar_2015)
+movie_list.append(school_of_rock)
+movie_list.append(ratatouille)
+movie_list.append(midnight_in_paris)
+
+# Generate and open the website using the list of movie objects
+fresh_tomatoes.open_movies_page(movie_list)
+
---
+++ media.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+class Video():
+	"""This class proviced a way to store information of general video"""
+	def __init__(self, title, duration):
+		self.title = title
+		self.duration = duration
+
+class Movie(Video):
+	"""This class provides a way to store movie related information"""
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+	def __init__(self, movie_title, movie_duration, movie_storyline, poster_image, trailer_youtube):
+		# Calling initializer of parent
+		Video.__init__(self, movie_title, movie_duration)
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
+
+	
